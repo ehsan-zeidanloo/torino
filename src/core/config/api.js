348683cd5b46bc082463e604d25c802d36e04b62
ehsan zeidanloo <ehsanzeidanloo@gmail.com>
2024-12-10 21:05:16 +0330

    created the project and created folder structur and config api ,developed cookie function

---
+++ src/core/config/api.js
@@ -0,0 +1,62 @@
+const { getCookie, setCookie } = require("../utils/cookie");
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (request) => {
+    const accessToken = getCookie("accessToken");
+    if (accessToken) {
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+
+  async (error) => {
+    const orginialRequest = error.config;
+    if (error.response.status === 401 && !originialRequest._retry) {
+      orginialRequest._retry = true;
+
+      const res = await getNewToken();
+      if (res?.response?.status === 201) {
+        setCookie("accessToken", res?.response?.data.accessToken, 30);
+        setCookie("refreshToken", res?.response?.data.refreshToken, 360);
+        return api(orginialRequest);
+      } else {
+        setCookie("accessToken", "", 0);
+        setCookie("refreshToken", "", 0);
+      }
+    }
+    return Promise.reject(error.response.data);
+  }
+);
+
+export default api;
+
+const getNewToken = async () => {
+  const refreshToken = getCookie("refreshToken");
+  if (!refreshToken) return;
+
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}auth/refresh-token`,
+      { refreshToken }
+    );
+    return { response };
+  } catch (error) {
+    return { error };
+  }
+};
